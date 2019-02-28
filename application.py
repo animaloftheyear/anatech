543740472f4823595b82d7d9ad6a1ae27f0349f7
Jin Chen <jin.chen.dectech@gmail.com>
2019-02-28 13:40:48 +0000

    contact page and about page

--- application.py
+++ application.py
@@ -16,3 +16,15 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/what")
+def what():
+    return render_template("what.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
